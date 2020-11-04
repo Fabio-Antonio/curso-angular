6dbe4deef6fa668e34d6f49b232953355a52ecc5
Fabio-Antonio <ing.fabio.a@gmail.com>
2020-11-03 20:32:27 -0600

    seccion-tres

---
+++ src/app/pages/progress/progress-is.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-is',
+  templateUrl: './progress-is.component.html',
+  styleUrls: ['./progress-is.component.css']
+})
+export class ProgressIsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
